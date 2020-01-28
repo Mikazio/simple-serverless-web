1f55943bc7af13dd9a0b6886def4432c4742f130
Varin Titarat <varin.titarat@gmail.com>
2020-01-28 23:53:43 +0700

    [Add]AWS-Cli scripts to do jobs

--- Create-Simple-S3.tf
+++ Create-Simple-S3.tf
@@ -4,16 +4,23 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_web" {
-    bucket = "varin-static-website.com"
-    acl    = "private"
-    policy = "${file("s3-policy.json")}"
-    
-    website {
-        index_document = "index.html"
-    }
+  bucket = "varin-static-website.com"
+  acl    = "private"
+  policy = "${file("s3-policy.json")}"
 
-    tags = {
-        Name    = "Static Bucket"
-        Environment = "Pre-Production"
-    }
+  website {
+    index_document = "index.html"
+  }
+
+  tags = {
+    Name        = "Static Bucket"
+    Environment = "Pre-Production"
+  }
+}
+
+resource "null_resource" "output_id" {
+  provisioner "local-exec" {
+    command = "aws s3 sync s3://wildrydes-us-east-1/WebApplication/1_StaticWebHosting/website s3://varin-static-website.com --region ap-southeast-1"
+  }
+    depends_on = ["aws_s3_bucket.static_web"]
 }
