fc2fe1b14130c77a7da7adb65271ab0288b12208
Varin Titarat <varin.titarat@gmail.com>
2020-01-29 22:06:21 +0700

    [Add]Cognito and Identity pool with facebook access

--- Create-Simple-S3.tf
+++ Create-Simple-S3.tf
@@ -18,6 +18,32 @@ resource "aws_s3_bucket" "static_web" {
   }
 }
 
+resource "aws_cognito_identity_pool" "user" {
+    identity_pool_name               = "user_pool"
+    allow_unauthenticated_identities = false
+}
+
+resource "aws_cognito_user_pool" "user_pool" {
+  name                     = "user_pool"
+  auto_verified_attributes = ["email"]
+}
+
+resource "aws_cognito_identity_provider" "provider" {
+  user_pool_id  = "${aws_cognito_user_pool.user_pool.id}"
+  provider_name = "Facebook"
+  provider_type = "Facebook"
+
+  provider_details = {
+    authorize_scopes = "email"
+    client_id        = "your client_id"
+    client_secret    = "your client_secret"
+  }
+
+  attribute_mapping = {
+    email    = "email"
+  }
+}
+
 resource "null_resource" "output_id" {
   provisioner "local-exec" {
     command = "aws s3 sync s3://wildrydes-us-east-1/WebApplication/1_StaticWebHosting/website s3://varin-static-website.com --region ap-southeast-1"
